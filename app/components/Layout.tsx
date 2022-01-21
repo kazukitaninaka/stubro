6a530f08b042123b858c08157b547f4f17290510
kazukitaninaka <onelive555@gmail.com>
2022-01-21 16:46:56 +0900

    remove antd and install tailwind

---
+++ app/components/Layout.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from "react";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex flex-col">
+      <header className="bg-sky-500 p-5 text-white">
+        <h1>STUBRO</h1>
+      </header>
+      <div>{children}</div>
+      <footer className="bg-sky-500 p-5 text-white">FOOTER</footer>
+    </div>
+  );
+}
