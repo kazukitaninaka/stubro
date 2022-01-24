c5c4b030309d957cd99e103c627cd997255d251d
kazukitaninaka <onelive555@gmail.com>
2022-01-24 17:29:29 +0900

    tiny fix in initializing firebase(Next.js problem)

--- app/utils/firebase.ts
+++ app/utils/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
@@ -10,7 +10,6 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export default app;
