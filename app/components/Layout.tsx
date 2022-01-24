b57a076906b6d50d83b513a58034af0462f16133
kazukitaninaka <onelive555@gmail.com>
2022-01-24 10:46:42 +0900

    fix footer at the bottom

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -2,13 +2,13 @@ import React, { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col min-h-screen">
       <header className="bg-sky-500 p-5 text-white">
         <h1>STUBRO</h1>
       </header>
-      <div className="bg-gray-50">
+      <main className="bg-gray-50 flex-1">
         <div className="container mx-auto py-5 px-3">{children}</div>
-      </div>
+      </main>
       <footer className="bg-sky-500 p-5 text-white">FOOTER</footer>
     </div>
   );
