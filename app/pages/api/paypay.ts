d4c8f42ad8a2168ac80faeff55acbac05d60c169
kazukitaninaka <onelive555@gmail.com>
2022-01-28 17:17:18 +0900

    deal with type of variables from paypay sdk

--- app/pages/api/paypay.ts
+++ app/pages/api/paypay.ts
@@ -1,10 +1,11 @@
 import PAYPAY from '@paypayopa/paypayopa-sdk-node';
+import { HttpsClientSuccess } from '@paypayopa/paypayopa-sdk-node/dist/lib/httpsClient';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { v4 as uuidv4 } from 'uuid';
 
 PAYPAY.Configure({
-  clientId: process.env.NEXT_PUBLIC_PAYPAY_API_KEY,
-  clientSecret: process.env.NEXT_PUBLIC_PAYPAY_SECRET,
+  clientId: process.env.NEXT_PUBLIC_PAYPAY_API_KEY!,
+  clientSecret: process.env.NEXT_PUBLIC_PAYPAY_SECRET!,
   merchantId: process.env.NEXT_PUBLIC_PAYPAY_MERCHANT_ID,
   productionMode: false,
 });
@@ -21,14 +22,15 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     codeType: 'ORDER_QR',
     orderDescription: "Mune's Favourite Cake",
     isAuthorization: false,
-    redirectUrl: 'http://localhost:3000/',
+    redirectUrl: 'http://localhost:3000/payment-completed',
     redirectType: 'WEB_LINK',
     userAgent,
   };
+  // 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1'
   // Calling the method to create a qr code
   PAYPAY.QRCodeCreate(payload, (response) => {
     console.log(response);
     // Printing if the method call was SUCCESS
-    res.status(201).json(JSON.parse(response.BODY));
+    res.status(201).json(JSON.parse((response as HttpsClientSuccess).BODY));
   });
 }
