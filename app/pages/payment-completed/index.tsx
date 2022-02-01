bc38c1ceb605e9253e115d5ed17b50503cc10e48
kazukitaninaka <onelive555@gmail.com>
2022-02-01 17:29:03 +0900

    adjust style of payment-completed page

--- app/pages/payment-completed/index.tsx
+++ app/pages/payment-completed/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { CheckCircleIcon } from '@heroicons/react/solid';
 import useConsultationDetailsSlice from '../../slices/consultationDetails/useConsultationDetailsSlice';
 
 export default function PaymentCompleted() {
@@ -10,13 +11,27 @@ export default function PaymentCompleted() {
     };
   }, []);
   return (
-    <div>
-      <h2>Payment completed</h2>
-      <p>詳細</p>
-      <p>メンター名：{consultationDetails.username}</p>
-      <p>希望日時：{consultationDetails.desirableDate}</p>
-      <p>コメント：{consultationDetails.comments}</p>
-      <p>
+    <div className='max-w-[650px] mx-auto'>
+      <CheckCircleIcon className='h-48 w-48 text-green-500 mx-auto' />
+      <p className='text-center text-4xl mb-5'>Payment completed!</p>
+      <p className='text-center text-xl mb-3'>お申し込み内容</p>
+      <table className='table-fixed mx-auto'>
+        <tbody className='divide-y divide-gray-200'>
+          <tr>
+            <td className='w-1/3 font-bold'>メンター名</td>
+            <td className='text-lg'>{consultationDetails.username}</td>
+          </tr>
+          <tr>
+            <td className='font-bold'>希望日時</td>
+            <td className='text-lg whitespace-pre-wrap'>{consultationDetails.desirableDate}</td>
+          </tr>
+          <tr>
+            <td className='font-bold'>コメント</td>
+            <td className='text-lg'>{consultationDetails.comments}</td>
+          </tr>
+        </tbody>
+      </table>
+      <p className='mt-5 text-lg'>
         メンターと日程を調整の上、決まり次第登録されたメールアドレスに日程とzoomリンクをお送りします。
       </p>
     </div>
