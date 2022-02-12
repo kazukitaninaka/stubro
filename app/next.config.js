0ebc67cb53ab2a13c18d8ff7c1e86c07d21bc8d4
kazukitaninaka <onelive555@gmail.com>
2022-02-13 00:25:10 +0900

    make proxy to tackle cors problem

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        // it can proxy calls to go api server
+        source: '/go-api/:path*',
+        destination: 'http://localhost:8080/:path*',
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
