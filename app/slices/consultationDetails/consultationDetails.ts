c0b32300b4a31ed762b64e19a8050fbebb8ec452
kazukitaninaka <onelive555@gmail.com>
2022-02-13 10:51:25 +0900

    change field name of cosultation: id to mentorId

--- app/slices/consultationDetails/consultationDetails.ts
+++ app/slices/consultationDetails/consultationDetails.ts
@@ -12,7 +12,7 @@ export type UserInput = {
 };
 
 export type ConsultationDetails = {
-  id: number | null;
+  mentorId: number | null;
   username: string;
   price: number;
   desirableDate: string;
@@ -20,7 +20,7 @@ export type ConsultationDetails = {
 };
 
 const initialState: ConsultationDetails = {
-  id: null,
+  mentorId: null,
   username: '',
   price: 0,
   desirableDate: '',
@@ -33,12 +33,12 @@ const consultationDetailsSlice = createSlice({
   reducers: {
     setMentor: (_, action: PayloadAction<Mentor>) => {
       const { id, username, price } = action.payload;
-      return { id, username, price, desirableDate: '', comments: '' };
+      return { mentorId: id, username, price, desirableDate: '', comments: '' };
     },
     setUserInput: (state, action: PayloadAction<UserInput>) => {
       const { desirableDate, comments } = action.payload;
       return {
-        id: state.id,
+        mentorId: state.mentorId,
         username: state.username,
         price: state.price,
         desirableDate,
