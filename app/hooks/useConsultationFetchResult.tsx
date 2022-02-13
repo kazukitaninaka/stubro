a361910254dea521d93a13aceae340392cd039d0
kazukitaninaka <onelive555@gmail.com>
2022-02-13 16:14:55 +0900

    enable posting consultation

---
+++ app/hooks/useConsultationFetchResult.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { Consultation, ConsultationApi } from '../api';
+import useConsultationDetailsSlice from '../slices/consultationDetails/useConsultationDetailsSlice';
+import useUserSlice from '../slices/user/useUserSlice';
+
+export default function useConsultationFetchResult() {
+    const [isRegisterSuccess, setIsRegisterSuccess] = useState<boolean>(false);
+    const { consultationDetails } = useConsultationDetailsSlice();
+    const { user } = useUserSlice();
+    useEffect(() => {
+        const consultation: Consultation = {
+            mentorId: consultationDetails.mentorId!,
+            userId: user.id!,
+            desirableDate: consultationDetails.desirableDate,
+            message: consultationDetails.message,
+        };
+
+        const api = new ConsultationApi();
+        api
+            .postConsultation(consultation)
+            .then(() => {
+                setIsRegisterSuccess(true);
+            })
+            .catch((e) => {
+                console.log(e);
+                setIsRegisterSuccess(false);
+            });
+    }, []);
+    return { isRegisterSuccess };
+}
--- app/pages/payment-completed/index.tsx
+++ app/pages/payment-completed/index.tsx
@@ -2,11 +2,15 @@ import React, { useEffect } from 'react';
 import { CheckCircleIcon } from '@heroicons/react/solid';
 import useConsultationDetailsSlice from '../../slices/consultationDetails/useConsultationDetailsSlice';
 import ContainerSm from '../../components/ContainerSm';
+import useConsultationFetchResult from '../../hooks/useConsultationFetchResult';
+import { useRouter } from 'next/router';
 
 export default function PaymentCompleted() {
   const { consultationDetails, setConsultationDetailsInitial } = useConsultationDetailsSlice();
-  useEffect(() => {
+  const { isRegisterSuccess } = useConsultationFetchResult();
+  const router = useRouter()
 
+  useEffect(() => {
     return () => {
       // ページを離れる時にpersistantのconsultationDetailsをリセット
       setConsultationDetailsInitial();
@@ -14,28 +18,37 @@ export default function PaymentCompleted() {
   }, []);
   return (
     <ContainerSm>
-      <CheckCircleIcon className='h-48 w-48 text-green-500 mx-auto' />
-      <p className='text-center text-4xl mb-5'>Payment completed!</p>
-      <p className='text-center text-xl mb-3'>お申し込み内容</p>
-      <table className='table-fixed mx-auto'>
-        <tbody className='divide-y divide-gray-200'>
-          <tr>
-            <td className='w-1/3 font-bold'>メンター名</td>
-            <td className='text-lg'>{consultationDetails.username}</td>
-          </tr>
-          <tr>
-            <td className='font-bold'>希望日時</td>
-            <td className='text-lg whitespace-pre-wrap'>{consultationDetails.desirableDate}</td>
-          </tr>
-          <tr>
-            <td className='font-bold'>メッセージ</td>
-            <td className='text-lg'>{consultationDetails.message}</td>
-          </tr>
-        </tbody>
-      </table>
-      <p className='mt-5 text-lg'>
-        メンターと日程を調整の上、決まり次第登録されたメールアドレスに日程とzoomリンクをお送りします。
-      </p>
+      {isRegisterSuccess ? (
+        <>
+          <CheckCircleIcon className='h-48 w-48 text-green-500 mx-auto' />
+          <p className='text-center text-4xl mb-5'>Payment completed!</p>
+          <p className='text-center text-xl mb-3'>お申し込み内容</p>
+          <table className='table-fixed mx-auto'>
+            <tbody className='divide-y divide-gray-200'>
+              <tr>
+                <td className='w-1/3 font-bold'>メンター名</td>
+                <td className='text-lg'>{consultationDetails.username}</td>
+              </tr>
+              <tr>
+                <td className='font-bold'>希望日時</td>
+                <td className='text-lg whitespace-pre-wrap'>{consultationDetails.desirableDate}</td>
+              </tr>
+              <tr>
+                <td className='font-bold'>メッセージ</td>
+                <td className='text-lg'>{consultationDetails.message}</td>
+              </tr>
+            </tbody>
+          </table>
+          <p className='mt-5 text-lg'>
+            メンターと日程を調整の上、決まり次第登録されたメールアドレスに日程とzoomリンクをお送りします。
+          </p>
+        </>
+      ) : (
+        <>
+          <p>エラーが発生しました。TOPページに戻ってからもう一度お試しください。</p>
+          <button className='mt-3 border bg-sky-500 text-white p-2 rounded-sm' onClick={() => router.push("/")}>TOP</button>
+        </>
+      )}
     </ContainerSm>
   );
 }
