ea3902b3c8e843b0aa34d647900b0b0719712345
kazukitaninaka <onelive555@gmail.com>
2022-02-16 20:49:45 +0900

    change page path for showing all mentors

---
+++ app/pages/home/index.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import Card from '../../components/Card';
+import ConfirmModal from '../../components/ConfirmModal';
+import { MentorApi, Mentor } from '../../api';
+
+const Home = ({ mentors }: { mentors: Mentor[] }) => {
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+    return (
+        <div>
+            <div className='flex flex-wrap gap-5'>
+                {mentors.map((mentor) => (
+                    <Card key={mentor.id} mentor={mentor} setIsModalOpen={setIsModalOpen} />
+                ))}
+            </div>
+            <ConfirmModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
+        </div>
+    );
+};
+
+export default Home;
+
+export async function getServerSideProps() {
+    const api = new MentorApi();
+    const res = await api.getMentors();
+    const mentors = res.data;
+
+    return {
+        props: {
+            mentors,
+        },
+    };
+}
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,33 +1,8 @@
-import { useState } from 'react';
-import Card from '../components/Card';
-import ConfirmModal from '../components/ConfirmModal';
-import { MentorApi, Mentor } from '../api';
+import React from 'react'
 
-const Home = ({ mentors }: { mentors: Mentor[] }) => {
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-
+export default function index() {
   return (
-    <div>
-      <div className='flex flex-wrap gap-5'>
-        {mentors.map((mentor) => (
-          <Card key={mentor.id} mentor={mentor} setIsModalOpen={setIsModalOpen} />
-        ))}
-      </div>
-      <ConfirmModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
-    </div>
-  );
-};
-
-export default Home;
-
-export async function getServerSideProps() {
-  const api = new MentorApi();
-  const res = await api.getMentors();
-  const mentors = res.data;
-
-  return {
-    props: {
-      mentors,
-    },
-  };
+    <div>index</div>
+  )
 }
+
