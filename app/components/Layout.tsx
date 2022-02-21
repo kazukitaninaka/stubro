cbafe37d4eaa362c02498dd345c8db37652a17ce
kazukitaninaka <onelive555@gmail.com>
2022-02-21 11:31:45 +0900

    change in Layout: set max-width and remove bg color of header and footer

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -11,30 +11,32 @@ export default function Layout({ children }: { children: ReactNode }) {
   const { isUserLoggedIn } = useUserSlice();
   const router = useRouter();
   return (
-    <div className='flex flex-col min-h-screen'>
-      <header className='bg-sky-500 p-5 text-white flex justify-between'>
-        <Link href='/'>
-          <h1 className='cursor-pointer'>STUBRO</h1>
-        </Link>
-        {isUserLoggedIn ? (
-          <UserCircleIcon
-            className='h-7 w-7 cursor-pointer'
-            onClick={() => router.push('/my-page')}
-          />
-        ) : (
-          <button
-            className='bg-white text-sky-500 border border-transparent rounded-md px-2 py-1 hover:bg-slate-100 flex items-center gap-x-1'
-            onClick={() => router.push('/login')}
-          >
-            <span>LOGIN</span>
-            <LoginIcon className='h-5 w-5' />
-          </button>
-        )}
-      </header>
-      <main className='bg-gray-50 flex-1'>
-        <div className='container mx-auto py-5 px-3'>{children}</div>
-      </main>
-      <footer className='bg-sky-500 p-5 text-white'>FOOTER</footer>
+    <div className='bg-gray-50'>
+      <div className='flex flex-col min-h-screen max-w-6xl mx-auto'>
+        <header className='p-5 flex justify-between'>
+          <Link href='/'>
+            <h1 className='cursor-pointer text-sky-500 text-xl font-semibold'>StuBro 🎓</h1>
+          </Link>
+          {isUserLoggedIn ? (
+            <UserCircleIcon
+              className='h-7 w-7 cursor-pointer'
+              onClick={() => router.push('/my-page')}
+            />
+          ) : (
+            <button
+              className='bg-sky-500 text-white border border-transparent rounded-md px-2 py-1 hover:bg-sky-600 flex items-center gap-x-1'
+              onClick={() => router.push('/login')}
+            >
+              <span>LOGIN</span>
+              <LoginIcon className='h-5 w-5' />
+            </button>
+          )}
+        </header>
+        <main className='flex-1'>
+          <div className='container mx-auto py-5 px-3'>{children}</div>
+        </main>
+        <footer className='p-5'>FOOTER</footer>
+      </div>
     </div>
   );
 }
