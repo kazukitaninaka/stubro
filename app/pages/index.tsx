6ae7d81be5dc0bb5e0fc7a59f414306ac98b0b26
kazukitaninaka <onelive555@gmail.com>
2022-02-21 11:38:33 +0900

    change in markup of LP

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -5,12 +5,13 @@ export default function index() {
   const router = useRouter()
   return (
     <>
-      <div className="flex flex-col sm:flex-row justify-evenly">
-        <div className="sm:w-1/3 mx-auto">
-          <h1 className="text-3xl lg:text-4xl sm:mt-24 font-medium"><span className="font-bold text-sky-500">StuBro</span>で
+      <div className="grid lg:grid-cols-12">
+        <Hangout className="lg:col-span-6 lg:order-last w-2/3 lg:w-full mx-auto" />
+        <div className="lg:col-span-6 mx-auto lg:mx-0 text-center lg:text-left">
+          <h1 className="text-4xl lg:text-5xl mt-12 lg:mt-24 font-medium"><span className="font-bold text-sky-500">StuBro</span>で
             <br />気軽に留学相談<br />はじめませんか</h1>
           <button
-            className='bg-sky-500 text-white py-2 px-3 rounded-md hover:bg-sky-600 mt-10 mx-auto'
+            className='bg-sky-500 text-white py-2 px-3 rounded-md hover:bg-sky-600 mt-5 lg:mt-10 mx-auto'
             onClick={() => router.push("/home")}
           >
             相談相手を探してみる
