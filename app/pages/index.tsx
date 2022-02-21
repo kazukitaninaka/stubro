ef0a8f8df2dcc9c73ba0354646ecc0c20b5a76db
kazukitaninaka <onelive555@gmail.com>
2022-02-21 10:47:02 +0900

    change position of button on LP

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -9,18 +9,17 @@ export default function index() {
         <div className="sm:w-1/3 mx-auto">
           <h2 className="text-3xl lg:text-4xl sm:mt-24 font-medium"><span className="font-bold text-sky-500">StuBro</span>で
             <br />気軽に留学相談<br />はじめませんか</h2>
+          <button
+            className='bg-sky-500 text-white py-2 px-3 rounded-md hover:bg-sky-600 mt-10 mx-auto'
+            onClick={() => router.push("/home")}
+          >
+            相談相手を探してみる
+          </button>
         </div>
         <Hangout className="h-full mt-5 sm:mt-0 w-[90%] sm:w-1/2 mx-auto" />
       </div>
 
-      <div className="flex justify-center">
-        <button
-          className='bg-sky-500 text-white py-2 px-3 rounded-md hover:bg-sky-600 mt-10 mx-auto'
-          onClick={() => router.push("/home")}
-        >
-          相談相手を探してみる
-        </button>
-      </div>
+
     </>
   )
 }
