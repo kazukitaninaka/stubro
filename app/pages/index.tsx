80cfd6447bf0eae98ebbea28998e503cf0d2eca2
kazukitaninaka <onelive555@gmail.com>
2022-02-21 11:09:22 +0900

    change tag on LP: h2 to h1

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -7,8 +7,8 @@ export default function index() {
     <>
       <div className="flex flex-col sm:flex-row justify-evenly">
         <div className="sm:w-1/3 mx-auto">
-          <h2 className="text-3xl lg:text-4xl sm:mt-24 font-medium"><span className="font-bold text-sky-500">StuBro</span>で
-            <br />気軽に留学相談<br />はじめませんか</h2>
+          <h1 className="text-3xl lg:text-4xl sm:mt-24 font-medium"><span className="font-bold text-sky-500">StuBro</span>で
+            <br />気軽に留学相談<br />はじめませんか</h1>
           <button
             className='bg-sky-500 text-white py-2 px-3 rounded-md hover:bg-sky-600 mt-10 mx-auto'
             onClick={() => router.push("/home")}
