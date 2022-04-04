60b09c460f8fbb71730a0670d020dfdea713bbea
kazukitaninaka <onelive555@gmail.com>
2022-04-04 18:30:09 +0900

    add: _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function MyDocument() {
+    const title = "StuBro"
+    const description = "留学経験者と気軽に話せるサービス"
+    return (
+        <Html>
+            <Head>
+                <title>{title}</title>
+                <meta property="og:title" content={title} />
+                <meta property="og:site_name" content={title} />
+                <meta name="description" content={description} />
+                <meta property="og:description" content={description} />
+                <meta property="og:image" content="https://emoji.beeimg.com/🎓/apple" />
+                <link rel="icon" type="image/png" href="https://emoji.beeimg.com/🎓/apple" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
