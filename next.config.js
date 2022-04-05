a5cfba271d761cfe729ce624301ea78a1661d006
kazukitaninaka <onelive555@gmail.com>
2022-04-05 11:20:32 +0900

    delete: unused config in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        // it can proxy calls to go api server
-        source: '/go-api/:path*',
-        destination: process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_API_SERVER_PROD : 'http://localhost:8080' + '/:path*',
-      },
-    ]
-  },
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
