7d3a1057fa83cddb24f46fd7620b97815b4a8d11
kazukitaninaka <onelive555@gmail.com>
2022-04-05 11:21:37 +0900

    update: have token in userSlice

--- hooks/useConsultationFetchResult.tsx
+++ hooks/useConsultationFetchResult.tsx
@@ -9,6 +9,7 @@ export default function useConsultationFetchResult() {
     const [isRegistering, setIsRegistering] = useState<boolean>(false);
     const { consultationDetails } = useConsultationDetailsSlice();
     const { user } = useUserSlice();
+
     useEffect(() => {
         setIsRegistering(true)
         const consultation: ConsultationRequest = {
@@ -19,25 +20,18 @@ export default function useConsultationFetchResult() {
         };
 
         const api = new ConsultationApi();
-        const auth = getAuth()
-        onAuthStateChanged(auth, async (user) => {
-            if (user) {
-                const token = await user.getIdToken()
-                api
-                    .postConsultation(consultation, { headers: { Authorization: `Bearer ${token}` } })
-                    .then(() => {
-                        setIsRegisterSuccess(true);
-                        setIsRegistering(false)
-                    })
-                    .catch((e) => {
-                        console.log(e);
-                        setIsRegisterSuccess(false);
-                        setIsRegistering(false)
-                    });
-            } else {
-                console.log("no user found")
-            }
-        })
+
+        api
+            .postConsultation(consultation, { headers: { Authorization: `Bearer ${user.token}` } })
+            .then(() => {
+                setIsRegisterSuccess(true);
+                setIsRegistering(false)
+            })
+            .catch((e) => {
+                console.log(e);
+                setIsRegisterSuccess(false);
+                setIsRegistering(false)
+            });
     }, []);
     return { isRegisterSuccess, isRegistering };
 }
